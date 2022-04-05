c150697ca05159261f77d43a3e2321f28031e44f
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-05 00:05:16 -0400

    commit tree 0d4cafd92a9ea3be7a5414c3d48461d71786f58c

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  let result
+  if (someNumber <= 400 ) {
+    result = 'This one is on me!'
+  } else if (someNumber < 2000) {
+    result = 'That will be twenty bucks.'
+  } else if (someNumber < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (someNumber > 2500) {
+    result = 'No can do.'}
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result
+  city === 'NYC' ? result = 'Ok, sounds good.' : result = 'No go.'
+  return result
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+let result
+
+switch (tip) {
+  case 'generous' : 
+    return result = 'Thank you so much.'
+    break
+  case 'not as generous' : 
+    return result = "Thank you."
+    break
+  default:
+    return result = 'Bye.'
+return result
+}
 }
